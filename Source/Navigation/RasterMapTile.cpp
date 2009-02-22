84d15a66ff0753f57eb3dc8c3a27124d581ad182
Hugo Vincent <hugo.vincent@gmail.com>
2009-02-22 14:20:31 +1300

    Add skeleton for RasterMapTile

---
+++ Source/Navigation/RasterMapTile.cpp
@@ -0,0 +1,19 @@
+/*
+ *  RasterMapTile.cpp
+ *  GlassCockpit
+ *
+ *  Created by Hugo Vincent on 22/02/09.
+ *  Copyright 2009 Hugo Vincent. All rights reserved.
+ *
+ */
+
+#include "RasterMapTile.h"
+
+namespace OpenGC {
+
+RasterMapTile::RasterMapTile()
+{
+
+}
+
+} // end namespace OpenGC
---
+++ Source/Navigation/RasterMapTile.h
@@ -0,0 +1,23 @@
+/*
+ *  RasterMapTile.h
+ *  GlassCockpit
+ *
+ *  Created by Hugo Vincent on 22/02/09.
+ *  Copyright 2009 Hugo Vincent. All rights reserved.
+ *
+ */
+
+#ifndef RasterMapTile_h
+#define RasterMapTile_h
+
+namespace OpenGC {
+
+class RasterMapTile
+{
+public:
+	RasterMapTile();
+};
+	
+} // end namespace OpenGC
+
+#endif // RasterMapTile_h
