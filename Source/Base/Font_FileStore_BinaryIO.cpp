c25dcf868af058c41f716e4a99ad67b0729ee4ef
Hugo Vincent <hugo.vincent@gmail.com>
2010-08-28 15:07:25 +0100

    Update all the copyright headers.

--- Source/Base/Font_FileStore_BinaryIO.cpp
+++ Source/Base/Font_FileStore_BinaryIO.cpp
@@ -1,11 +1,24 @@
-/*
- *  Font_FileStore_BinaryIO.cpp
- *  GlassCockpit
- *
- *  Created by Hugo Vincent on 9/02/09.
- *  Copyright 2009 Hugo Vincent. All rights reserved.
- *
- */
+/*=========================================================================
+
+  Copyright (c) 2005-2010 Hugo Vincent <hugo.vincent@gmail.com>
+  All rights reserved.
+  
+  This project is distributed under the terms of the GNU General Public License
+  Version 3 <http://www.gnu.org/licenses/gpl.html>.
+  
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, specifically version 3 of the License.
+  
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+  
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=========================================================================*/
 
 #include "Debug.h"
 #include <math.h>
--- Source/Gauges/PFDOverlay.cpp
+++ Source/Gauges/PFDOverlay.cpp
@@ -1,17 +1,24 @@
 /*=========================================================================
 
-  OpenGC - The Open Source Glass Cockpit Project
-  Please see our web site at http://www.opengc.org
-
-  Albatross UAV Project - http://www.albatross-uav.org
-
-  Copyright (c) 2006 Hugo Vincent.
+  Copyright (c) 2005-2010 Hugo Vincent <hugo.vincent@gmail.com>
   All rights reserved.
+  
+  This project is distributed under the terms of the GNU General Public License
+  Version 3 <http://www.gnu.org/licenses/gpl.html>.
+  
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, specifically version 3 of the License.
+  
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+  
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-  This file is distributed under the terms of the GNU General Public
-  License version 2.0. See included COPYING file for details.
-
- =========================================================================*/
+=========================================================================*/
 
 #include "Globals.h"
 #include "GLHeaders.h"
