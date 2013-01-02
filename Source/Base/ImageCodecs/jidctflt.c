8f621fddd190e8a9bcdaa18fad294713bbd7f1cf
Hugo Vincent <hugo.vincent@gmail.com>
2013-01-02 17:21:47 +0100

    Minor (and quickly hacked) patch to make it build on recent versions of Xcode.

--- Source/Base/ImageCodecs/jidctflt.c
+++ Source/Base/ImageCodecs/jidctflt.c
@@ -80,7 +80,7 @@
 
 #define DEQUANTIZE(coef,quantval)  (((FAST_FLOAT) (coef)) * (quantval))
 
-#if 1 && defined(__GNUC__) && (defined(__i686__) || defined(__x86_64__))
+#if 0 && defined(__GNUC__) && (defined(__i686__) || defined(__x86_64__))
 
 static inline unsigned char descale_and_clamp(int x, int shift)
 {
